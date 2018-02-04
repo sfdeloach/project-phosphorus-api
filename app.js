3f863c5e7c25e18995d2251e3e0ab9f44595a5e3
Steven DeLoach <sfdeloach@gmail.com>
2018-02-04 18:12:35 -0500

    complete officer CRUD ops

--- app.js
+++ app.js
@@ -25,18 +25,21 @@ app.use(bodyParser.json({
 }));
 
 // Allow CORS on ExpressJS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
+  "use strict";
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
-app.get('/api/officers', function(req, res) {
+// Officer API starts here /////////////////////////////////////////////////////
+
+app.get('/api/officers', function (req, res) {
   "use strict";
   console.log(`${new Date()} - GET /api/officers request received`);
 
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, function (err, client) {
     if (err) {
       console.log(`${new Date()} - ` + noConnectMessage);
       console.log(err);
@@ -47,8 +50,7 @@ app.get('/api/officers', function(req, res) {
       });
     } else {
       const db = client.db(dbName);
-
-      db.collection(officersCollection).find().toArray(function(err, result) {
+      db.collection(officersCollection).find().toArray(function (err, result) {
         if (err) {
           console.log(`${new Date()} - ` + dbErrorMessage);
           console.log(err);
@@ -65,13 +67,47 @@ app.get('/api/officers', function(req, res) {
   });
 });
 
-app.post('/api/officers/new', function(req, res) {
+app.get('/api/officers/:id', function (req, res) {
+  "use strict";
+  const id = req.params.id;
+  console.log(`${new Date()} - GET /api/officers/${id} request received`);
+  const errorOfc = {
+    "name": {
+      "last": "??? Unable to retrieve officer",
+      "first": "??? Unable to retrieve officer"
+    }
+  };
+
+  MongoClient.connect(url, function (err, client) {
+    if (err) {
+      console.error(err);
+      res.json(errorOfc);
+    } else {
+      console.log(`${new Date()} - Successful connection to the database`);
+      const db = client.db(dbName);
+      console.log(`${new Date()} - Database client set to '${dbName}'`);
+      db.collection(officersCollection).findOne({ _id: new mongodb.ObjectID(id) }, function (err, result) {
+        if (err) {
+          console.log(`${new Date()} - An error occurred during the 'findOne()' operation`);
+          console.error(err);
+          res.json(errorOfc);
+        } else {
+          console.log(`${new Date()} - Successful retrieval: ${result._id}`);
+          res.json(result);
+        }
+      });
+    }
+  });
+
+});
+
+app.post('/api/officers/new', function (req, res) {
   "use strict";
   console.log(`${new Date()} - POST /api/officers/new request received`);
 
   const newOfficer = req.body.officer;
 
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, function (err, client) {
     if (err) {
       console.log(`${new Date()} - ` + noConnectMessage);
       res.json({
@@ -83,7 +119,7 @@ app.post('/api/officers/new', function(req, res) {
       console.log(`${new Date()} - Successful connection to db ${dbName}`);
       const db = client.db(dbName);
 
-      db.collection(officersCollection).insert(newOfficer, function(err, result) {
+      db.collection(officersCollection).insert(newOfficer, function (err, result) {
         assert.equal(null, err);
         const successInsertMessage = `${result.insertedCount} officer added to ${officersCollection}`;
         console.log(`${new Date()} - ` + successInsertMessage);
@@ -97,12 +133,51 @@ app.post('/api/officers/new', function(req, res) {
   });
 });
 
-app.delete('/api/officers/:id', function(req, res){
+app.put('/api/officers/:id', function (req, res) {
+  "use strict";
+  const id = req.params.id;
+  console.log(`${new Date()} - PUT /api/officers/${id} request received`);
+
+  const updateOfc = req.body.officer;
+  delete updateOfc._id; // must remove _id field or replaceOne() will fail
+
+  MongoClient.connect(url, function (err, client) {
+    if (err) {
+      console.log(`${new Date()} - ` + noConnectMessage);
+      res.json({
+        "error": true,
+        "message": noConnectMessage,
+        "lines": 0
+      });
+    } else {
+      const db = client.db(dbName);
+
+      db.collection(officersCollection).replaceOne({ _id: new mongodb.ObjectID(id) }, updateOfc,
+        function (err, result) {
+          if (err) {
+            console.error("An error occurred during the replaceOne() operation!");
+            console.error(err);
+          } else {
+            const successUpdateMessage = `Successful update of officer with id: ${updateOfc._id}`;
+            console.log(`${new Date()} - ` + successUpdateMessage);
+            res.json({
+              "error": false,
+              "message": successUpdateMessage,
+              "lines": 1
+            });
+          }
+        }
+      );
+    }
+  });
+});
+
+app.delete('/api/officers/:id', function (req, res) {
   "use strict";
   const id = req.params.id;
   console.log(`${new Date()} - DELETE /api/officers/${id} request received`);
 
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, function (err, client) {
     if (err) {
       console.log(`${new Date()} - ` + noConnectMessage);
       res.json({
@@ -114,7 +189,7 @@ app.delete('/api/officers/:id', function(req, res){
       console.log(`${new Date()} - Successful connection to db ${dbName}`);
       const db = client.db(dbName);
 
-      db.collection(officersCollection).deleteOne({_id: new mongodb.ObjectID(id)}, function(err, result) {
+      db.collection(officersCollection).deleteOne({ _id: new mongodb.ObjectID(id) }, function (err, result) {
         assert.equal(null, err);
         const successDeleteMessage = `${result.deletedCount} officer deleted from ${officersCollection}`;
         console.log(`${new Date()} - ` + successDeleteMessage);
@@ -126,15 +201,15 @@ app.delete('/api/officers/:id', function(req, res){
       });
     }
   });
-
-
 });
 
-app.get('/api/events', function(req, res) {
+// Events API starts here //////////////////////////////////////////////////////
+
+app.get('/api/events', function (req, res) {
   "use strict";
   console.log(`${new Date()} - GET /api/events request received`);
 
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, function (err, client) {
     if (err) {
       console.log(`${new Date()} - ` + noConnectMessage);
       console.log(err);
@@ -146,7 +221,7 @@ app.get('/api/events', function(req, res) {
     } else {
       const db = client.db(dbName);
 
-      db.collection(eventsCollection).find().toArray(function(err, result) {
+      db.collection(eventsCollection).find().toArray(function (err, result) {
         if (err) {
           console.log(`${new Date()} - ` + dbErrorMessage);
           console.log(err);
@@ -163,7 +238,7 @@ app.get('/api/events', function(req, res) {
   });
 });
 
-app.post('/api/events', function(req, res) {
+app.post('/api/events', function (req, res) {
   "use strict";
   const passcode = req.body.passcode;
   const payload = req.body.payload;
@@ -173,7 +248,7 @@ app.post('/api/events', function(req, res) {
 
   if (passcode === accessCodes.alpha) {
     console.log(`${new Date()} - Proper passcode was provided, access to API granted`);
-    MongoClient.connect(url, function(err, client) {
+    MongoClient.connect(url, function (err, client) {
       if (err) {
         console.log(`${new Date()} - ` + noConnectMessage);
         res.json({
@@ -185,7 +260,7 @@ app.post('/api/events', function(req, res) {
         console.log(`${new Date()} - Successful connection to db ${dbName}`);
         const db = client.db(dbName);
 
-        db.collection(eventsCollection).insertMany(payload, function(err, result) {
+        db.collection(eventsCollection).insertMany(payload, function (err, result) {
           assert.equal(null, err);
           const successInsertMessage = `${result.insertedCount} documents added to ${eventsCollection}`;
           console.log(`${new Date()} - ` + successInsertMessage);
