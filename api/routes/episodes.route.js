8dc6da9fd5262a804f4aee93a1baed41fedf6de1
Steven DeLoach <sfdeloach@gmail.com>
2018-03-08 16:45:03 -0500

    episode rework

--- api/routes/episodes.route.js
+++ api/routes/episodes.route.js
@@ -14,87 +14,92 @@ router.use((req, res, next) => {
   next();
 });
 
-// RESTful API
-
-// Read - GET
-
+// Read - GET //////////////////////////////////////////////////////////////////
 // api/episodes/ - return all episodes based on req.body.query (defaults {})
-// api/episodes/call/:id - return one call by :id = call.eventNbr
+router.get('/', (req, res) => {
+  keg.query = req.body.query || {};
+  command.connect(keg)
+  .then(keg => command.find(keg))
+  .then(keg => res.json(keg.findResult))
+  .catch(err => {
+    console.error(err);
+    res.json(new ErrJsonRes(
+      logger.message(req), err, keg
+    ));
+  });
+});
+
+// api/episodes/call/:eventNbr - return one call by :eventNbr = call.eventNbr
+router.get('/call/:eventNbr', (req, res) => {
+  keg.query = { 'call.eventNbr': +req.params.eventNbr };
+  command.connect(keg)
+  .then(keg => command.find(keg))
+  .then(keg => res.json(keg.findResult))
+  .catch(err => {
+    console.error(err);
+    res.json(new ErrJsonRes(
+      logger.message(req), err, keg
+    ));
+  });
+});
+
 // api/episodes/report/:id - find report by :id = report.caseNbr
+
 // api/episodes/:id - return one episode by :id = episode.ObID or call.eventNbr
 
-// Create - POST
-
+// Create - POST ///////////////////////////////////////////////////////////////
 // api/episodes/ - insert one or many episodes
-// api/episodes/call/units/:id - insert one unit on :id = episode.ObID or call.eventNbr
-// api/episodes/report/:id - insert one report on :id = episode.ObID or call.eventNbr
-
-// Update - PUT
-
-// api/episodes/:id - update one episode by :id = episode.ObID or call.eventNbr
-
-// Delete - DELETE
-
-// api/episodes/ - deletes all episodes
-// api/episodes/call/units/:id - delete one unit by :id = officer.ObID or officer.deptID
-// api/episodes/report/:id - delete one report by :id = report.caseNbr
-// api/episodes/:id - delete one episode by :id = episode.ObID or call.eventNbr
-
-
-// return the results of a query object
-router.get('/find', (req, res) => {
-  keg.query = req.body.query || {};
-  command.connect(keg)
-    .then(keg => command.find(keg))
-    .then(keg => res.json(keg.findResult))
-    .catch(err => {
-      console.error(err);
-      res.json(new ErrJsonRes(
-        logger.message(req), err, keg
-      ));
-    });
-});
-
-// insert an array of episodes
-router.post('/insert-many', (req, res) => {
+router.post('/', (req, res) => {
   keg.documents = req.body.episodes;
   command.connect(keg)
-    .then(keg => command.insertMany(keg))
-    .then(keg => res.json(keg.insertManyResult))
-    .catch(err => {
-      console.error(err);
-      res.json(new ErrJsonRes(
-        logger.message(req), err, keg
-      ));
-    });
+  .then(keg => command.insertMany(keg))
+  .then(keg => res.json(keg.insertManyResult))
+  .catch(err => {
+    console.error(err);
+    res.json(new ErrJsonRes(
+      logger.message(req), err, keg
+    ));
+  });
 });
 
-router.put('/replace-one', (req, res) => {
-  keg.query = req.body.query || {};
+// api/episodes/call/units/:id - insert one unit on :id = episode.ObID or call.eventNbr
+
+// api/episodes/report/:id - insert one report on :id = episode.ObID or call.eventNbr
+
+// Update - PUT ////////////////////////////////////////////////////////////////
+// api/episodes/:id - update one episode by :id = episode.ObID or call.eventNbr
+router.put('/:id', (req, res) => {
   keg.document = req.body.episode;
   command.connect(keg)
-    .then(keg => command.replaceOne(keg))
-    .then(keg => res.json(keg.replaceOneResult))
-    .catch(err => {
-      console.error(err);
-      res.json(new ErrJsonRes(
-        logger.message(req), err, keg
-      ));
-    });
+  .then(keg => command.replaceOne(keg))
+  .then(keg => res.json(keg.replaceOneResult))
+  .catch(err => {
+    console.error(err);
+    res.json(new ErrJsonRes(
+      logger.message(req), err, keg
+    ));
+  });
 });
 
-// remove episodes matching the provided query
-router.delete('/remove', (req, res) => {
+// Delete - DELETE /////////////////////////////////////////////////////////////
+// api/episodes/ - deletes all episodes
+router.delete('/', (req, res) => {
   keg.query = req.body.query || {};
   command.connect(keg)
-    .then(keg => command.remove(keg))
-    .then(keg => res.json(keg.removeResult))
-    .catch(err => {
-      console.error(err);
-      res.json(new ErrJsonRes(
-        logger.message(req), err, keg
-      ));
-    });
+  .then(keg => command.remove(keg))
+  .then(keg => res.json(keg.removeResult))
+  .catch(err => {
+    console.error(err);
+    res.json(new ErrJsonRes(
+      logger.message(req), err, keg
+    ));
+  });
 });
+
+// api/episodes/call/units/:id - delete one unit by :id = officer.ObID or officer.deptID
+
+// api/episodes/report/:id - delete one report by :id = report.caseNbr
+
+// api/episodes/:id - delete one episode by :id = episode.ObID or call.eventNbr
 
 module.exports = router;
