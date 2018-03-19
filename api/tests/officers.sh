48c76d3e3684bc4e554a2a76c33fdbde0c6d9183
Steven DeLoach <sfdeloach@gmail.com>
2018-03-19 15:54:11 -0400

    officer testing

--- api/tests/officers.sh
+++ api/tests/officers.sh
@@ -4,30 +4,30 @@ clear
 now=$(date)
 echo $now
 
+R='\033[0;31m' # red
+G='\033[0;32m' # green
 O='\033[0;33m' # orange
 P='\033[1;35m' # pink
-G='\033[0;32m' # green
-R='\033[0;31m' # red
 C='\033[0m' # clear
 
 url="localhost:3000/api/officers"
 
 # delete the officer collection
 printf "${P}Test #50:${C} DELETE $url..."
-testValue1=19
+expected01=19
 curl --silent -o ./output/test-50.result.json \
   --request DELETE \
   --url $url
 length=$(cat ./output/test-50.result.json \
   | jq --raw-output '.n')
-if [ $length == $testValue1 ]
-then printf "${G}SUCCESS...$testValue1 == $length${C}\n";
-else printf "${R}FAILURE...$testValue1 != $length${C}\n";
+if [ $length == $expected01 ]
+then printf "${G}SUCCESS...$expected01 == $length${C}\n";
+else printf "${R}FAILURE...$expected01 != $length${C}\n";
 fi
 
 # insertMany officers
 printf "${P}Test #51:${C} POST $url..."
-testValue1=18
+expected01=18
 curl --silent -o ./output/test-51.result.json \
   --request POST \
   --url $url \
@@ -35,14 +35,14 @@ curl --silent -o ./output/test-51.result.json \
   --data "@./input/officers.json"
 length=$(cat ./output/test-51.result.json \
   | jq --raw-output '.result.n')
-if [ $length == $testValue1 ];
-then printf "${G}SUCCESS...$testValue1 == $length${C}\n";
-else printf "${R}FAILURE...$testValue1 != $length${C}\n";
+if [ $length == $expected01 ];
+then printf "${G}SUCCESS...$expected01 == $length${C}\n";
+else printf "${R}FAILURE...$expected01 != $length${C}\n";
 fi
 
 # insert one officer
 printf "${P}Test #52:${C} POST $url..."
-testValue1='Delrusso'
+expected01='Delrusso'
 curl --silent -o ./output/test-52.result.json \
   --request POST \
   --url $url \
@@ -50,28 +50,47 @@ curl --silent -o ./output/test-52.result.json \
   --data "@./input/officer.json"
 lastName=$(cat ./output/test-52.result.json \
   | jq --raw-output '.ops[0].name.last')
-if [ $lastName == $testValue1 ];
-then printf "${G}SUCCESS...$testValue1 == $lastName${C}\n";
-else printf "${R}FAILURE...$testValue1 != $lastName${C}\n";
+if [ $lastName == $expected01 ];
+then printf "${G}SUCCESS...$expected01 == $lastName${C}\n";
+else printf "${R}FAILURE...$expected01 != $lastName${C}\n";
 fi
 
 # get all episodes
 printf "${P}Test #53:${C} GET $url..."
-testValue1='Raquan'
-testValue2=19
+expected01='Raquan'
+expected02=19
 curl --silent -o ./output/test-53.result.json \
   --request GET \
   --url $url
 lastName=$(cat ./output/test-53.result.json \
   | jq --raw-output '.[17].name.last')
 length=$(cat ./output/test-53.result.json \
-  | jq '. | length')
-if [ $lastName == $testValue1 ] && [ $length == $testValue2 ];
-then printf "${G}SUCCESS...$testValue1 == $lastName and $testValue2 == $length${C}\n";
-else printf "${R}FAILURE...$testValue1 != $lastName and $testValue2 != $length${C}\n";
+  | jq --raw-output '. | length')
+if [ $lastName == $expected01 ] && [ $length == $expected02 ];
+then printf "${G}SUCCESS...$expected01 == $lastName and $expected02 == $length${C}\n";
+else printf "${R}FAILURE...$expected01 != $lastName and $expected02 != $length${C}\n";
 fi
 
+# save _id of an officer
+objectID=$(cat ./output/test-53.result.json \
+  | jq --raw-output '.[7]._id')
+
 # get an officer by _id
+printf "${P}Test #54:${C} GET $url..."
+expected01='Raquan'
+expected02=19
+curl --silent -o ./output/test-54.result.json \
+  --request GET \
+  --url $url
+lastName=$(cat ./output/test-54.result.json \
+  | jq --raw-output '.[17].name.last')
+length=$(cat ./output/test-54.result.json \
+  | jq '. | length')
+if [ $lastName == $expected01 ] && [ $length == $expected02 ];
+then printf "${G}SUCCESS...$expected01 == $lastName and $expected02 == $length${C}\n";
+else printf "${R}FAILURE...$expected01 != $lastName and $expected02 != $length${C}\n";
+fi
+
 # replace an officer by _id
 # get an officer by query
 # get officers by squad
